fa49b96d899a3f81c66d2882d3e1a7a7f21303dc
Priyank Gupta <priyaaank@gmail.com>
2012-11-12 18:05:55 +0530

    Added splash and main screen

--- src/com/barefoot/trackon/SplashScreenActivity.java
+++ src/com/barefoot/trackon/SplashScreenActivity.java
@@ -1,15 +1,27 @@
 package com.barefoot.trackon;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreenActivity extends Activity
 {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+		@Override
+		public void onCreate(Bundle savedInstanceState) {
+				super.onCreate(savedInstanceState);
+				setContentView(R.layout.splash_screen);
+
+				Handler handler = new Handler();
+				handler.postDelayed(new Runnable() {
+						@Override
+						public void run() {
+								finish();
+								Intent intent = new Intent(SplashScreenActivity.this, HomeActivity.class);
+								SplashScreenActivity.this.startActivity(intent);
+						}
+
+				}, 2000);
+
+		}
 }
